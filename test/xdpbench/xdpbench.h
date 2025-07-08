e393fbb3bdcd2c3b31d391589dfc2dd50ab33321
huajianwang <huajianwang@microsoft.com>
2025-07-08 04:48:03 +0000

    MOD: Merge download and upload path

--- test/xdpbench/xdpbench.h
+++ test/xdpbench/xdpbench.h
@@ -22,12 +22,12 @@
 #define STR_OF(x) SHALLOW_STR_OF(x)
 
 #define ALIGN_DOWN_BY(length, alignment) \
-    ((ULONG_PTR)(length)& ~(alignment - 1))
+	((ULONG_PTR)(length)& ~(alignment - 1))
 #define ALIGN_UP_BY(length, alignment) \
-    (ALIGN_DOWN_BY(((ULONG_PTR)(length)+alignment - 1), alignment))
+	(ALIGN_DOWN_BY(((ULONG_PTR)(length)+alignment - 1), alignment))
 
 #define STRUCT_FIELD_OFFSET(structPtr, field) \
-    ((UCHAR *)&(structPtr)->field - (UCHAR *)(structPtr))
+	((UCHAR *)&(structPtr)->field - (UCHAR *)(structPtr))
 
 #define DEFAULT_UMEM_SIZE 65536
 #define DEFAULT_UMEM_CHUNK_SIZE 4096
@@ -50,19 +50,22 @@
 #define DEFAULT_REQ_RATE 0
 #define DEFAULT_FRAME_RATE 10000
 #define DEFAULT_REQ_SAMPLES 2000000
+
+const int kResetLatencyCounterMark = -1;
+const int kFinalPacketMark = -1;
 //-huajianwang:eelat
 
 #define printf_error(...) \
-    fprintf(stderr, __VA_ARGS__)
+	fprintf(stderr, __VA_ARGS__)
 
 #define printf_verbose(format, ...) \
-    if (verbose) { LARGE_INTEGER Qpc; QueryPerformanceCounter(&Qpc); printf("Qpc=%llu " format, Qpc.QuadPart, __VA_ARGS__); }
+	if (verbose) { LARGE_INTEGER Qpc; QueryPerformanceCounter(&Qpc); printf("Qpc=%llu " format, Qpc.QuadPart, __VA_ARGS__); }
 
 #define ABORT(...) \
-    printf_error(__VA_ARGS__); exit(1)
+	printf_error(__VA_ARGS__); exit(1)
 
 #define ASSERT_FRE(expr) \
-    if (!(expr)) { ABORT("(%s) failed line %d\n", #expr, __LINE__);}
+	if (!(expr)) { ABORT("(%s) failed line %d\n", #expr, __LINE__);}
 
 #if DBG
 #define VERIFY(expr) assert(expr)
@@ -77,6 +80,6 @@
 
 INT64
 QpcToUs64(
-    INT64 Qpc,
-    INT64 QpcFrequency
-);
+		INT64 Qpc,
+		INT64 QpcFrequency
+	 );
